1c55a6955e81957f5c114b4f22fcbc8b798a0084
Leen Zreaq <leenzreaq21@gmail.com>
2026-06-19 16:33:37 +0300

    Initial commet in the shop

---
+++ src/main/java/com/shop/ClotheShop/ClotheShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.ClotheShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClotheShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClotheShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/ClotheShop/ClotheShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.ClotheShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClotheShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
