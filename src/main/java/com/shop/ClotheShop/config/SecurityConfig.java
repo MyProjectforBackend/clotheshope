189deeb6e0161b57c411c7bbdfd9f1ab8d94f728
Leen Zreaq <leenzreaq21@gmail.com>
2026-07-09 18:26:45 +0300

    change the jpa

--- src/main/java/com/shop/ClotheShop/config/SecurityConfig.java
+++ src/main/java/com/shop/ClotheShop/config/SecurityConfig.java
@@ -12,8 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
-
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
@@ -65,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
 
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
 
     }
 
--- src/main/java/com/shop/ClotheShop/domain/Address.java
+++ src/main/java/com/shop/ClotheShop/domain/Address.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,17 +13,18 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Address {
     @Id
+    // make the id auto-incremented
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     @ManyToOne
     @JoinColumn(name = "user_id")
+    // make the user field a foreign key to the User entity
     private User user;
-    
+
     private String city;
     private String country;
 }
